29fed66aec80ebfb41384292150cfea158c101ae
Stephanie Pamperin <steph.pamperin@gmail.com>
2015-08-17 09:53:54 -0500

    broken links

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
-var currentPage = 0;
+//var currentPage = 0;
 
-$('.book')
-.on('click', '.active', nextPage)
-.on('click', '.flipped', prevPage);
+//$('.book')
+//.on('click', '.flipped', prevPage);
 
-$('.book').hammer().on("swipeleft", nextPage);
-$('.book').hammer().on("swiperight", prevPage);
+//$('.book').hammer().on("swipeleft", nextPage);
+//$('.book').hammer().on("swiperight", prevPage);
 
 function prevPage() {
   
@@ -27,3 +26,22 @@ function nextPage() {
     
     
 }
+$(document).ready(function(){
+    var currentPage=5
+    if (window.location.hash == '#pagefive') {
+        $('section').removeClass('active');
+        $('#pagefive').addClass('active');
+    }
+});
+var zCascade=function(pages, active, maxZ) {
+    var offset=1;
+    for(var i=2*active-1; i>=0; i--){
+        $(pages[i]).css("z-index",maxZ+offset);
+        offset--;
+    }
+    offset=1;
+    for(var i=2*active; i<pages.length; i++){
+        $(pages[i]).css("z-index",maxZ+offset);
+        offset--;
+    }
+};
